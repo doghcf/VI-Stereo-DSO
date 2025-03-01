eeacf761fd2f93a5869da26dc4982bf1a8459826
doghcf <522563047@qq.com>
2025-03-01 21:41:04 +0800

    run euroc

--- run_euroc.bash
+++ run_euroc.bash
@@ -1,12 +1,22 @@
 #!/bin/bash
 #usage: ./run.bash
 
-dataname="00"
+# dataname="MH_01_easy"
+# dataname="MH_02_easy"
+# dataname="MH_03_medium"
+# dataname="MH_04_difficult"
+dataname="MH_05_difficult"
+# dataname="V1_01_easy"
+# dataname="V1_02_medium"
+# dataname="V1_03_difficult"
+# dataname="V2_01_easy"
+# dataname="V2_02_medium"
+# dataname="V2_03_difficult"
 
 # run dso
 ./build/bin/dso_dataset \
-	files0=/home/bjergsen/data/kitti/${dataname}/image_0 \
-	files1=/home/bjergsen/data/euroc/${dataname}/image_1 \
+	files0=/home/bjergsen/data/euroc/${dataname}/mav0/cam0/data \
+	files1=/home/bjergsen/data/euroc/${dataname}/mav0/cam1/data \
 	calib0=/home/bjergsen/VI-Stereo-DSO/calib/euroc/cam0.txt \
 	calib1=/home/bjergsen/VI-Stereo-DSO/calib/euroc/cam1.txt \
 	T_stereo=/home/bjergsen/VI-Stereo-DSO/calib/euroc/T_C0C1.txt \
@@ -18,5 +28,5 @@ dataname="00"
 	preset=0 mode=1 \
 	quiet=1 nomt=1 \
 	savefile_tail=nt_${dataname}\
-	use_stereo=1\
+	use_stereo=1 nogui=1 \
 	imu_weight=6 imu_weight_tracker=0.6 stereo_weight=0.5
